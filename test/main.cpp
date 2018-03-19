0792271b83139950c1c58c3ebc6dec83ba283e8b
John Fercher <johnathanfercher22@gmail.com>
2018-03-18 23:08:04 -0300

    forçando retorno para quebrar

---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by johnathan on 18/03/18.
+//
+
+#include <googletest/googletest/include/gtest/gtest.h>
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return 1;//RUN_ALL_TESTS();
+}
