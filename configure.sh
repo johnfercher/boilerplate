ad825f89234145ee104bede886ef3c892784c227
John Fercher <johnathanfercher22@gmail.com>
2018-03-18 23:51:09 -0300

    voltado para o correto

--- configure.sh
+++ configure.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#
-# This file is part of the VSS-SDK project.
-#
-# This Source Code Form is subject to the terms of the GNU GENERAL PUBLIC LICENSE,
-# v. 3.0. If a copy of the GPL was not distributed with this
-# file, You can obtain one at http://www.gnu.org/licenses/gpl-3.0/.
-#
 
 CREATE_BUILD_FOLDER_IF_DOESNT_EXIST() {
     if [ ! -d build ]
@@ -23,4 +16,3 @@ INSTALL() {
 }
 
 INSTALL;
-
--- test.sh
+++ test.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#
-# This file is part of the VSS-SDK project.
-#
-# This Source Code Form is subject to the terms of the GNU GENERAL PUBLIC LICENSE,
-# v. 3.0. If a copy of the GPL was not distributed with this
-# file, You can obtain one at http://www.gnu.org/licenses/gpl-3.0/.
-#
-
 
 EXIT_IF_AN_ERROR_OCCURRED() {
     LAST_CODE=$?;
@@ -25,4 +17,3 @@ TEST() {
 }
 
 TEST;
-
